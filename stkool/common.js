3b721b31b6aa8e06d8e845f9ac72a1137e491382
helfard <helfard@hotmail.com>
2022-05-04 00:21:04 +0900

    6.110504
    ランク94まで

--- stkool/common.js
+++ stkool/common.js
@@ -8,7 +8,7 @@ const log = msg => {
 const CONFIG = {
     // 基本設定
     name: 'Submersible Maker',
-    ver: '6.100424',
+    ver: '6.110504',
     auther: 'Omochi Kinako (Chocobo)',
     url: 'https://jp.finalfantasyxiv.com/lodestone/character/17471563/blog/4205382/',
 }
